4deb972bfe74769b61f0e6f11166343292d74a2d
Sung Bae <singsang2@gmail.com>
2018-10-27 04:12:35 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,4 @@
-# Write your code here.
+d# Write your code here.
 
 def word_substituter(tweet)
   dict = {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
